000ac51a90d8a840b1a92a4fd42d773397230753
congasix <trancong208@gmail.com>
2020-10-10 12:27:45 +0900

    Add files via upload

---
+++ ged4py/algorithm/graph_edit_dist.py
@@ -0,0 +1,58 @@
+# -*- coding: UTF-8 -*-
+from __future__ import print_function
+from ged4py.algorithm.abstract_graph_edit_dist import AbstractGraphEditDistance
+from ged4py.algorithm.edge_edit_dist import EdgeEditDistance
+from ged4py.graph.edge_graph import EdgeGraph
+import sys
+from networkx import __version__ as nxv
+
+
+
+def compare(g1, g2, print_details=False):
+    ged = GraphEditDistance(g1, g2)
+
+    if print_details:
+        ged.print_matrix()
+    gedresult = ged.normalized_distance()
+    if gedresult > 1:
+        gedresult = gedresult/2
+    
+    return gedresult
+
+
+class GraphEditDistance(AbstractGraphEditDistance):
+
+    def __init__(self, g1, g2):
+        AbstractGraphEditDistance.__init__(self, g1, g2)
+
+    def substitute_cost(self, node1, node2):
+        return self.relabel_cost(node1, node2) + self.edge_diff(node1, node2)
+
+    def relabel_cost(self, node1, node2):
+        if node1 == node2:
+            return 0.
+        else:
+            return 0.
+
+    def delete_cost(self, i, j, nodes1):
+        if i == j:
+            return 1
+        return sys.maxsize
+
+    def insert_cost(self, i, j, nodes2):
+        if i == j:
+            return 1
+        else:
+            return sys.maxsize
+
+    def pos_insdel_weight(self, node):
+        return 1
+
+    def edge_diff(self, node1, node2):
+        edges1 = list(self.g1.edge[node1].keys()) if float(nxv) < 2 else list(self.g1.edges(node1))
+        edges2 = list(self.g2.edge[node2].keys()) if float(nxv) < 2 else list(self.g2.edges(node2))
+        if len(edges1) == 0 or len(edges2) == 0:
+            return max(len(edges1), len(edges2))
+
+        edit_edit_dist = EdgeEditDistance(EdgeGraph(node1,edges1), EdgeGraph(node2,edges2))
+        return edit_edit_dist.normalized_distance()
